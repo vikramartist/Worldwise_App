f149fd5367b8e5360d2b57da9fa6e2a05e966372
vikramartist <vikrampaiv2604@gmail.com>
2023-12-21 19:48:05 +0530

    Add new Hooks folder with useGeolocation custom hook

---
+++ src/hooks/useGeolocation.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+
+function useGeolocation() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+
+  function getPosition() {
+    if (!navigator.geolocation) {
+      setError("Your Browser does not support geolocation");
+    }
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (err) => {
+        setError(err.message);
+        setIsLoading(false);
+      }
+    );
+  }
+
+  return { isLoading, position, error, getPosition };
+}
+
+export default useGeolocation;
